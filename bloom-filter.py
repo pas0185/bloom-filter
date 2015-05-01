57162f59cc801a2e0c6ae0810df3dd815dc74d2d
Patrick Sheehan <psheehan0301@gmail.com>
2015-05-01 14:53:25 -0500

    More pythonic code on iterations, remove some logging

--- bloom-filter.py
+++ bloom-filter.py
@@ -1,17 +1,25 @@
 __author__ = 'Patrick Sheehan'
 
 # Sources:
-# Bitarray Documentation:
+#
+# Bitarray
 # https://pypi.python.org/pypi/bitarray/
 #
-# MurmurHash3 Documentation:
+# MurmurHash3
 # https://pypi.python.org/pypi/mmh3/2.0
+#
+# Natural Language Toolkit
+# http://www.nltk.org/
+#
+# Piazza - TAMU CSCE 438
 #
 
 from bitarray import bitarray
 import mmh3
+import nltk
 
-class BloomFiler:
+
+class BloomFilter:
 
 
     def __init__(self, num_bits, num_hashes):
@@ -32,9 +40,9 @@ class BloomFiler:
         # "To add an element, feed it to each of the k hash functions to get k array positions.
         # Set the bits at all these positions to 1"
 
-        for seed in [i for i in range(self.num_hashes)]:
+        for seed in range(self.num_hashes):
             # Modulus for remainder if hash is larger than bit array
-            hash = mmh3.hash(element, seed) % self.num_bits
+            hash = mmh3.hash(element.lower(), seed) % self.num_bits
             self.bit_array[hash] = 1
 
         pass
@@ -45,18 +53,17 @@ class BloomFiler:
         # From Wikipedia:
         # "Feed it [the element] to each of the k hash functions to get k array positions.
 
-        for seed in [i for i in range(self.num_hashes)]:
-            hash = mmh3.hash(element, seed) % self.num_bits
+        for seed in range(self.num_hashes):
+            hash = mmh3.hash(element.lower(), seed) % self.num_bits
 
             # If any of the bits at these positions is 0, the element is definitely not in the set.
             # If it were, then all the bits would have been set to 1 when it was inserted.
             if self.bit_array[hash] == 0:
-                print 'The element %s is definitely not in the set' % element
+                # print 'The element %s is definitely not in the set' % element
                 return False
 
         # If all are 1, then either the element is in the set, or the bits have by chance been set to 1
         # during the insertion of other elements resulting in a false positive."
-        print 'The element %s is probably in the set' % element
         return True
 
     def read_training_file(self, file_name):
